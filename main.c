48564ae347565916bf1e4c3070e5d2f0de30363e
Frog Chen <satgo1546@yeah.net>
2017-02-25 17:18:17 +0800

    Enrich main.c

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 /* main.c */
+
+/* library */
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+/* definition */
+#include "read_config.c"
+#include "invoke.c"
+
+/* main */
 int main(int argc, char* argv[]) {
+	read_config();
+	for (int i = 1; i < argc; i++) {
+		invoke(argv[i]);
+	}
 	return 0;
 }
