e91628c93619d8933a7eee4f9761b584d7305dcb
Frog Chen <satgo1546@yeah.net>
2017-02-25 19:36:48 +0800

    Create everything.c

---
+++ everything.c
@@ -0,0 +1,33 @@
+/* everything.c */
+
+/* library */
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#ifdef _MSC_VER
+	#define bool int
+	#define true 1
+	#define false 0
+#else
+	#include <stdbool.h>
+#endif
+
+/* utilities */
+#include "error.c"
+#include "fncheck.c"
+
+/* platform */
+#ifdef _WIN32
+	#include "win32.c"
+#else
+	#include "posix.c"
+#endif
+
+/* definition */
+#include "read_config.c"
+#include "invoke.c"
+
+/* main */
+#include "main.c"
--- main.c
+++ main.c
@@ -1,32 +1,4 @@
 /* main.c */
-
-/* library */
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-
-/* platform */
-#ifdef _MSC_VER
-	#define bool int
-	#define true 1
-	#define false 0
-#else
-	#include <stdbool.h>
-#endif
-#ifdef _WIN32
-	#include "win32.c"
-#else
-	#include <unistd.h>
-	#include "posix.c"
-#endif
-
-/* definition */
-#include "error.c"
-#include "read_config.c"
-#include "invoke.c"
-
-/* main */
 int main(int argc, char* argv[]) {
 	int i;
 	type_dir* dir = dir_open("./*");
