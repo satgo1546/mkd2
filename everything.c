5122af2cd645eca6dd2daad01976d8a056e28d82
Frog Chen <satgo1546@yeah.net>
2017-03-11 15:57:16 +0800

    Use libanib

--- everything.c
+++ everything.c
@@ -1,31 +1,8 @@
 /* everything.c */
 
-/* library */
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#ifdef _MSC_VER
-	#define bool int
-	#define true 1
-	#define false 0
-#else
-	#include <stdbool.h>
-#endif
-
-/* utilities */
-#include "fncheck.c"
-
-/* platform */
-#ifdef _WIN32
-	#include "win32.c"
-#else
-	#include "posix.c"
-#endif
+#include "anib.h"
 
 /* definition */
-#include "error.c"
 #include "read_config.c"
 #include "invoke.c"
 
